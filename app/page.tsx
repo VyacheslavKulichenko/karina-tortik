19def4cb958e6b7aa739eba760ce020da9bf702b
Vyacheslav Kulychenko <vyacheslav.kulychenko@amedia.ua>
2026-01-24 16:55:28 +0200

    refactoring start screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,33 @@
-import React from "react";
-import PreviewPage from "./preview/page";
+import ColorSwitcher from "@/components/headers/ColorSwitcher";
+import Header1 from "@/components/headers/Header1";
+import Logo from "@/components/headers/Logo";
+import About from "@/components/homes/home-3/About";
+import BottomBackground from "@/components/homes/home-3/BottomBackground";
+import Contact from "@/components/homes/home-3/Contact";
+import Hero from "@/components/homes/home-3/Hero";
+import Portfolios from "@/components/homes/home-3/Portfolios";
+import Resume from "@/components/homes/home-3/Resume";
+import Services from "@/components/homes/home-3/Services";
 import { Metadata } from "next";
 export const metadata: Metadata = {
-  title:
-    "Preview || Blayden - Personal Portfolio & Resume React Nextjs Template",
+  title: "Blayden - Personal Portfolio & Resume React Nextjs Template",
   description: "Blayden - Personal Portfolio & Resume React Nextjs Template",
 };
 export default function page() {
   return (
     <>
-      <PreviewPage />
+      <Header1 />
+      <Logo />
+      <ColorSwitcher />
+      <main id="page-content" className="page-content">
+        <Hero />
+        <Portfolios />
+        <About />
+        <Services />
+        <Resume />
+        <Contact />
+      </main>
+      <BottomBackground />
     </>
   );
 }
